94c4700fd0148da4bc3444913813a95a9e88ba7b
pmdice <patrickmohr9@gmail.com>
2021-05-31 09:31:44 +0200

    Finalized

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,29 @@
 // Any code you will write for your website (in the future!) should go here
+
+$(document).ready(function(){
+	$('#on').on('change',function(){
+    	if(this.checked){
+            $("#priceOneText").text("Yearly Package");
+    		$("#priceOne").text("$25/Month");
+            /* ? Gap */
+            $("#priceTwoText").text("Yearly Package");
+            $("#priceTwo").text("$55/Month");
+            /* ? Gap */
+            $("#priceThreeText").text("Yearly Package");
+            $("#priceThree").text("$70/Month");
+    	}
+        else{
+            $("#priceOneText").text("Monthly Package");
+        	$("#priceOne").text("$30/Month");
+            /* Gap */
+            $("#priceTwoText").text("Monthly Package");
+            $("#priceTwo").text("$60/Month");
+            /* Gap */
+            $("#priceThreeText").text("Monthly Package");
+            $("#priceThree").text("$80/Month");
+        }
+    });
+});
+
+
+
